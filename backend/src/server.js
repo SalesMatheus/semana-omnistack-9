70bf50f4df425d321dea5b9a6fca021904fea599
Matheus <matheus.carvalho.sales@gmail.com>
2019-10-02 21:57:41 -0300

    rota para inserir novo usuario

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const routes = require('./routes');
 
 const app = express();
@@ -6,4 +7,13 @@ const app = express();
 app.use(express.json());
 app.use(routes);
 
-app.listen(3333);
+// Inociando o DB
+mongoose.connect(
+    'mongodb://localhost:27017/nodeapi',
+    {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    }
+);
+
+app.listen(3001);
