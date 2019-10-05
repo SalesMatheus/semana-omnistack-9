8ef40db33f95791f45b0129393c857afc7cfb310
Matheus <matheus.carvalho.sales@gmail.com>
2019-10-05 01:02:33 -0300

    rota para dashboard

--- backend/src/models/Spot.js
+++ backend/src/models/Spot.js
@@ -9,6 +9,14 @@ const SpotSchema = new mongoose.Schema({
         type: mongoose.Schema.Types.ObjectId,
         ref: 'User'
     }
+}, {
+    toJSON: {
+        virtuals: true,
+    }
 });
+
+SpotSchema.virtual('thunbnaill_url').get(function () {
+    return `http://localhost:3001/files/${this.thunbnaill}`
+})
 
 module.exports = mongoose.model('Spot', SpotSchema);
--- backend/src/server.js
+++ backend/src/server.js
@@ -1,16 +1,11 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
+const path = require('path');
 
 const routes = require('./routes');
 
 const app = express();
-
-//Setar parametro para permitir acessos a api
-app.use(cors());
-
-app.use(express.json());
-app.use(routes);
 
 // Inociando o DB
 mongoose.connect(
@@ -20,5 +15,13 @@ mongoose.connect(
         useUnifiedTopology: true,
     }
 );
+
+//Setar parametro para permitir acessos a api
+app.use(cors());
+
+app.use(express.json());
+app.use('/files', express.static(path.resolve(__dirname, '..', 'uploads')));
+app.use(routes);
+
 
 app.listen(3001);
