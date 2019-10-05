296fc5b27a80a7b6c6b5a92f8b95c6e2c7a0570f
Matheus <matheus.carvalho.sales@gmail.com>
2019-10-04 21:24:09 -0300

    Criando rotas

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -5,38 +5,3 @@ const api = axios.create({
 })
 
 export default api;
-
-
-// var http = require('http');
-
-// var options = {
-//     'method': 'POST',
-//     'hostname': 'localhost',
-//     'path': '/sessions',
-//     'headers': {
-//         'Content-Type': 'application/json'
-//     }
-// };
-
-// var req = http.request(options, function (res) {
-//     var chunks = [];
-
-//     res.on("data", function (chunk) {
-//         chunks.push(chunk);
-//     });
-
-//     res.on("end", function (chunk) {
-//         var body = Buffer.concat(chunks);
-//         console.log(body.toString());
-//     });
-
-//     res.on("error", function (error) {
-//         console.error(error);
-//     });
-// });
-
-// var postData = "{\n\t\"email\":\"matheus@gmail.com\"\n}";
-
-// req.write(postData);
-
-// req.end();
