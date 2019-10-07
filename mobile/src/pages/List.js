ba9a507d037851675bb0e7c3531adbdc34c036c1
Matheus <matheus.carvalho.sales@gmail.com>
2019-10-06 22:12:37 -0300

    Adicionando imagens na pagina de listagem

--- mobile/src/pages/List.js
+++ mobile/src/pages/List.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { SafeAreaView, StyleSheet, Image, AsyncStorage } from 'react-native';
+import { SafeAreaView, ScrollView, StyleSheet, Image, AsyncStorage } from 'react-native';
 
 import SpotList from '../components/SpotList';
 
@@ -22,7 +22,9 @@ export default function List() {
         <SafeAreaView style={StyleSheet.container}>
             <Image style={styles.logo} source={logo} />
 
-            <SpotList />
+            <ScrollView>
+                {techs.map(tech => <SpotList key={tech} tech={tech} />)}
+            </ScrollView>
         </SafeAreaView>
     )
 }
